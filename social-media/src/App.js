da829e981897bb738e91018a72579a17933a6998
Refije <Refijefejzullahu02@gmail.com>
2024-04-17 13:03:41 +0200

    error fixing

--- social-media/src/App.js
+++ social-media/src/App.js
@@ -1,45 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import jwtDecode from "jwt-decode";
-import { logInWithToken, selectUser } from "./store/slices/authSlice";
+import { useSelector } from "react-redux";
+import { selectUser } from "./store/slices/authSlice";
+import Login from "./pages/Login";
 
 function App() {
-  const dispatch = useDispatch();
   const loggedInUser = useSelector(selectUser);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token && !loggedInUser) {
-      const decodedToken = jwtDecode(localStorage.getItem("token"));
-      const isExpired = () => {
-        const dateNow = Date.now() / 1000;
-        return dateNow > decodedToken.exp;
-      };
-      if (!isExpired()) {
-        const fetchUserWithToken = async () => {
-          try {
-            const data = {
-              /* Your fetched user data */
-            };
-            dispatch(logInWithToken(data));
-          } catch (error) {
-            console.error("Error fetching user with token:", error);
-          }
-        };
-
-        fetchUserWithToken();
-      }
-    }
-  }, [dispatch, loggedInUser]);
 
   return (
     <Router>
-      <div className="App">
-        <AppRoutes />
-      </div>
+      <div className="App">{loggedInUser ? <AppRoutes /> : <Login />}</div>
     </Router>
   );
 }
